305fe964f67e9b90a40d0cac89279e73e06c24f1
Viasdeski <vitor.viasdeski@hotmail.com>
2024-04-15 16:47:30 -0300

    Crud completo

--- crud.js
+++ crud.js
@@ -12,12 +12,40 @@ class GerenciadorPessoa {
         this.pessoas.push(pessoa);
     }
 
+    atualizarPessoa(id, dadosAtaulizacao) {
+        const pessoa = this.pessoas.find(p => p.id === id);
+        if(pessoa){
+            Object.assign(pessoa, dadosAtaulizacao)
+        }
+    }
+
+    deletarPessoa(id) {
+        const pessoa = this.pessoas.findIndex(p => p.id === id);
+        if(pessoa !== -1) this.pessoas.splice(pessoa, 1);
+    }
+
     listarPessoas(){
         return this.pessoas
     }
 }
 
 
+//TESTES REALIZADOS
+
 gerenciador = new GerenciadorPessoa()
-gerenciador.cadastrarPessoa({'nome': 'Vitor', 'idade': 33, 'cidade': 'Czo'})
+
+const pessoa1 = {'nome': 'Joao', 'idade': 33, 'cidade': 'Porto Alegre'}
+const pessoa2 = {'nome': 'Ana', 'idade': 20, 'cidade': 'Roma'}
+
+gerenciador.cadastrarPessoa(pessoa1)
+gerenciador.cadastrarPessoa(pessoa2)
+
+gerenciador.atualizarPessoa(pessoa1.id, {cidade:'Brasilia'})
+
+gerenciador.deletarPessoa(pessoa2.id)
+
 console.log(gerenciador.listarPessoas())
+
+
+
+
